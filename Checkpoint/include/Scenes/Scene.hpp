e7a2e15ab0beae29680d159baf81db53c844af63
sebastian borquez <sebastian.borquez.g@gmail.com>
2017-08-16 06:14:33 -0300

    bugfix: iostream not include in scene.hpp

--- Checkpoint/include/Scenes/Scene.hpp
+++ Checkpoint/include/Scenes/Scene.hpp
@@ -1,7 +1,8 @@
 #ifndef __Scene__
-#define __Scene__ 
+#define __Scene__
 
 #include <string>
+#include <iostream>
 #include <vector>
 
 class Scene;
@@ -32,12 +33,12 @@ class BaseScene : public Scene
 {
 public:
     BaseScene(const std::string ID) : Scene(ID) { sceneID = ID; }
-    /*BaseScene(std::string id, std::vector<GameObject*> a_pObjects) 
+    /*BaseScene(std::string id, std::vector<GameObject*> a_pObjects)
         : sceneID(id),
             m_pObjects(a_pObjects)
     {
     }*/
-    
+
     ~BaseScene();
 
     virtual void update();
@@ -45,22 +46,22 @@ public:
     virtual void handleEvents();
 
     virtual void render();
-    
-    virtual bool onExit() { 
+
+    virtual bool onExit() {
         SDL_Log(" Chao, fui la escena %s", sceneID.c_str());
         return true;
     }
-    
-    virtual bool onEnter() { 
+
+    virtual bool onEnter() {
         SDL_Log(" Hola, soy la escena %s", sceneID.c_str());
         return true;
     }
 
     inline std::string getSceneID() const { return sceneID; }
 
-protected:    
+protected:
     //std::vector<GameObject*> m_pObjects;
     std::string sceneID;
 };
 
-#endif              
+#endif
